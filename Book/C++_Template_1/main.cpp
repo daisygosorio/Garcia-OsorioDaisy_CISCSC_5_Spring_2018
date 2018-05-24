cae67f5fac401b4af4886d0889f990712eb038f2
Daisygosorio@gmail.com <Daisygosorio@gmail.com>
2018-05-24 14:02:57 -0700

    Signed-off-by: Daisygosorio@gmail.com <Daisygosorio@gmail.com>

---
+++ Book/C++_Template_1/main.cpp
@@ -0,0 +1,38 @@
+/* 
+ * File:   main.cpp
+ * Author: Daisy Garcia-Osorio
+ * Created on May 3, 2018, 9:40 AM
+ * Purpose:  
+ */
+
+//System Libraries Here
+#include <iostream>// I/O LIbrary 
+#include <cmath>//Math Library
+#include <iomanip>//Formatting Library
+#include <cstdlib>//exit () function
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+   
+    
+    //Input or initialize values Here
+   
+    
+    //Process/Calculations Here
+   
+    
+    //Output Located Here
+   
+    //Exit
+    return 0;
+}
---
+++ Book/Chapter_6_Functions/Gaddis_8thEd_Ch.6_Program_1_Calling_a_Function/main.cpp
@@ -0,0 +1,47 @@
+/* 
+ * File:   main.cpp
+ * Author: Daisy Garcia-Osorio
+ * Created on May 24, 2018, 11:00 AM
+ * Purpose:  This program has two functions 
+ *           Main and displayMessage
+ */
+
+//System Libraries Here
+#include <iostream>// I/O LIbrary 
+#include <cmath>//Math Library
+#include <iomanip>//Formatting Library
+#include <cstdlib>//exit () function
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+void displayMessage();//Function Header ---> void displayMessage
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+   
+    
+    //Input or initialize values Here
+   
+    
+    //Process/Calculations Here
+   
+    
+    //Output Located Here
+    cout<<"Hello from Main Function"<<endl;
+    displayMessage();//<---This is a Function Call
+   
+    //Exit
+    return 0;
+}
+//Here is the function void displayMessage.
+void displayMessage(){
+   
+    cout<<"Hello from displayMessage"<<endl;
+
+}
